1a5f162ad8a101966e48f1038ba85221f65ce48d
eunchae619 <eunchae619@naver.com>
2020-04-18 04:34:53 +0900

    네오 삭제

--- do0.js
+++ do0.js
@@ -2,7 +2,7 @@
         // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
 
         // the link to your model provided by Teachable Machine export panel
-        const URL = "https://teachablemachine.withgoogle.com/models/vREOa4J5k/";
+        const URL = "https://teachablemachine.withgoogle.com/models/459TuPpDI/";
 
 
         let model, webcam, labelContainer, maxPredictions;
